bd8c49361561099edffc11bb437abec7202f2e0c
sikanderc <sikanderchowhan@gmail.com>
2017-09-18 09:56:41 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  attr_reader :name, :hometown, :favorite_song
+
+  get '/name' do
+    "My name is #{name}"
+  end
+
+  get '/hometown' do
+    "My hometown is #{hometown}"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is #{favorite_song}"
+  end
+
 end
